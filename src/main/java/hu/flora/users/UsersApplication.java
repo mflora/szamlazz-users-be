febb066edf61a39f85e54c82ba259e4999553a08
Flóra <flosztya@yahoo.com>
2024-04-05 07:09:30 +0200

    backend modifications

--- src/main/java/hu/flora/users/UsersApplication.java
+++ src/main/java/hu/flora/users/UsersApplication.java
@@ -1,11 +1,34 @@
 package hu.flora.users;
 
+import hu.flora.users.entites.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 
 @SpringBootApplication
 public class UsersApplication {
+
+	@Bean
+	public RepositoryRestConfigurer corsConfigurer() {
+		return new RepositoryRestConfigurer() {
+
+			@Override
+			public void configureRepositoryRestConfiguration(
+					RepositoryRestConfiguration config,
+					CorsRegistry cors) {
+				cors.addMapping("/**").allowedOrigins("*");
+
+				config.setDefaultMediaType(MediaType.APPLICATION_JSON);
+				config.useHalAsDefaultJsonMediaType(false);
+				config.exposeIdsFor(User.class);
+			}
+		};
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(UsersApplication.class, args);
--- src/main/java/hu/flora/users/repositories/UserRepository.java
+++ src/main/java/hu/flora/users/repositories/UserRepository.java
@@ -2,9 +2,8 @@ package hu.flora.users.repositories;
 
 import hu.flora.users.entites.User;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @CrossOrigin(origins = "*")
-public interface UserRepository extends PagingAndSortingRepository<User, Long>, CrudRepository<User, Long> {
+public interface UserRepository extends CrudRepository<User, Long> {
 }
